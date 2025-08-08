fea8fbe78c6e6e10c703392db2729decc0e739d0
Melanie Tolvett S <melanietolvett@gmail.com>
2025-08-08 16:51:46 -0400

    feat: added cors middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const { PORT } = require('./config')
 const uploadDocumentRouter = require('./router/uploadDocumentRouter')
 
 app.use(express.json())
+app.use(cors())
 app.use('/api/documents', uploadDocumentRouter)
 
 app.listen(PORT, () => {
