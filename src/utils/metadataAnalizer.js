dc5a369838e695748b17546ebd23953dcba5b80f
Melanie Tolvett S <melanietolvett@gmail.com>
2025-08-10 16:32:17 -0400

    refactor: metadataAnalizer function to improve it

--- src/utils/metadataAnalizer.js
+++ src/utils/metadataAnalizer.js
@@ -2,7 +2,9 @@ const { exiftool } = require("exiftool-vendored")
 
 const metadataAnalizer = async (filePath) => {
     const fileResults = {
+        filePath,
         valid: true,
+        score: 0,
         signals: [],
 
     }
@@ -12,49 +14,59 @@ const metadataAnalizer = async (filePath) => {
 
         // Check for specific metadata signals
 
-        //software
-        if (metadata.Software && /(Photoshop|GIMP|Word|Paint)/i.test(metadata.Software)) {
-            fileResults.valid = false
+        // suspicious software
+        if (metadata.Software && /(Photoshop|GIMP|Word|Paint|Illustrator)/i.test(metadata.Software)) {
+            fileResults.score += 3
             fileResults.signals.push(`Software de edición detectado: ${metadata.Software}`)
         }
-        // dates
+
+        // create date missing and modify date missing
+        if (!metadata.CreateDate) {
+            fileResults.score += 1
+            fileResults.signals.push("Falta fecha de creación (CreateDate)")
+        }
+
+        if (!metadata.ModifyDate) {
+            fileResults.score += 1
+            fileResults.signals.push("Falta fecha de modificación (ModifyDate)")
+        }
+
+        // modify date > create date
         if (metadata.CreateDate && metadata.ModifyDate) {
             const createDate = new Date(metadata.CreateDate)
             const modifyDate = new Date(metadata.ModifyDate)
             if (modifyDate > createDate) {
-                fileResults.valid = false
+                fileResults.score += 2
                 fileResults.signals.push(`Fecha de modificación posterior a la de creación: ${modifyDate} vs ${createDate} respectivamente`)
             }
         }
 
-        // empty essential fields
-        const essentialFields = ["Make", "Model", "CreateDate"]
-        const missingFields = essentialFields.filter(field => !metadata[field])
-
-        if (missingFields.length > 0) {
+        // calculated score
+        if (fileResults.score >= 4) {
             fileResults.valid = false
-            fileResults.signals.push(`Faltan metadatos esenciales: ${missingFields.join(", ")}`)
+            fileResults.signals.push(`Puntuación alta de sospecha: ${fileResults.score}`)
+        } else {
+            fileResults.valid = true
+            fileResults.signals.push(`Puntuación baja de sospecha: ${fileResults.score}`)
         }
 
-        exiftool.end()
 
     } catch (error) {
-        exiftool.end()
-        fileResults.valid = false;
-        fileResults.signals.push(`Error al leer metadatos: ${error.message}`);
+        fileResults.valid = false
+        fileResults.signals.push(`Error al leer metadatos: ${error.message}`)
         console.error(error)
     }
     return fileResults
 
 }
 
-
-(async () => {
-    const result = await metadataAnalizer('uploads/20250510_130650.jpg');
-    console.log(result);
-})()
-
-// (async () => {
-//     const result = await metadataAnalizer('uploads/document-1754686192710.jpg');
-//     console.log(result);
-// })()
+// Promise.all([
+//     metadataAnalizer('uploads/doc01034320250402103748.pdf'),
+//     metadataAnalizer('uploads/20250510_130650.jpg'),
+//     metadataAnalizer('uploads/document-1754686192710.jpg'),
+//     metadataAnalizer('uploads/REVISIÓN CAMIÓN .jpg'),
+//     metadataAnalizer('uploads/XF-3607 (1).jpg')
+// ]).then(results => {
+//     console.log(results)
+//     exiftool.end()
+// })
