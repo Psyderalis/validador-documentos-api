08853b47c7031a39d1ad19e38d2bf0d91de51533
Melanie Tolvett S <melanietolvett@gmail.com>
2025-08-13 16:40:41 -0400

    feat: incorporate openCV, add detectForgery for imgs

---
+++ src/utils/openCV.mjs
@@ -0,0 +1,22 @@
+import cvReadyPromise from "@techstark/opencv-js";
+
+async function loadOpenCv_4_10() {
+  const cv = cvReadyPromise;
+  return new Promise((resolve) => {
+    cv.onRuntimeInitialized = () => {
+      console.log("OpenCV.js is ready!");
+      resolve({ cv });
+    };
+  });
+}
+
+async function loadOpenCv_4_11() {
+  const cv = await cvReadyPromise;
+  console.log("OpenCV.js is ready!");
+  return { cv };
+}
+
+export async function getOpenCv() {
+  // return loadOpenCv_4_10();
+  return loadOpenCv_4_11();
+}
